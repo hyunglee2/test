9f4184b9ab19305760f85a77c4ca3c136edca192
hyunglee2 <hihylee2@gmail.com>
2023-02-09 10:40:37 +0900

    8

--- m_pg_handler.js
+++ m_pg_handler.js
@@ -51,7 +51,7 @@ function runConfetti() {
 //페이지 로드 시 전체 동작 코드 (DOM 로딩 후 동작)
 $(document).ready(function () {
     firstPageLoader();
-    $("#wrap").addClass("zoomIn");
+    $("#page_wrap").addClass("zoomIn");
 });
 
 
